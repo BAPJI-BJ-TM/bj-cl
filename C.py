0e2d15c54bdd703decad4ef3e141e6eb5edf0365
ARIF <115367469+BAPJI-BJ-TM@users.noreply.github.com>
2022-12-20 20:11:25 +0600

    Create C.py

---
+++ C.py
@@ -0,0 +1,27 @@
+import os
+lg='''
+
+             \033[1;33m____  ___    ____    ______
+            / __ )/   |  / __ \    / /  _/
+           / __  / /| | / /_/ /_  / // /  
+          / /_/ / ___ |/ ____/ /_/ // /   
+         /_____/_/  |_/_/    \____/___/   
+         _______________________________⠀⠀
+         \033[1;36mWELCOME TO BAP JI + COMPILATIOn '''
+
+
+
+print(lg)
+a=int(input("   \033[1;31mFIRST NUMBER + ="))
+b=int(input("   SECOND NUMBER + = "))
+p="="
+i=" ? "                                                                                   
+t= "ANSWER   ="
+q="Question ="
+os.system("clear")
+s="+"
+print(lg)
+print("\033[1;32m")
+print(q,a,s,b,  i)
+
+print(t,a,s,b,p,a+b)
